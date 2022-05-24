414d6dda2a1e350b8beda8cf07ca6ade26b29ccd
KevinGarciaCortez <105743455+KevinGarciaCortez@users.noreply.github.com>
2022-05-24 11:19:33 -0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingresa un numero");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa otro numero");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(num1 / num2); }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+               
+                Console.WriteLine(e.Message);
+            }
+            finally
+            { }
+            Console.ReadKey();
+        }
+            
+        }
+    }
+
